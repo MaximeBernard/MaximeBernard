247ffb598ee77754f47ee969602f56a6a89f2d57
Maxime Bernard <MaximeBernard@users.noreply.github.com>
2025-01-13 00:57:20 +0100

    fix: add prefix for images

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/MaximeBernard',
+  assetPrefix: '/MaximeBernard',
 };
 
 export default nextConfig;
